97467f2bc961e56e35adf5dd99d419658b0ee97f
silcreteantelope <ballmax23@gmail.com>
2021-10-12 12:39:00 -0400

    Fix white page bug and create components
    White page bug was fixed by loading the main component.
    
    Components added so people know which to work on.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EditBlogComponent } from './edit-blog/edit-blog.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { EditSocialComponent } from './edit-social/edit-social.component';
 import { NavComponent } from './nav/nav.component';
+import { FanfileComponent } from './fanfile/fanfile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +18,18 @@ import { NavComponent } from './nav/nav.component';
     EditBlogComponent,
     EditProfileComponent,
     EditSocialComponent,
-    NavComponent
+    NavComponent,
+    FanfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+	BrowserModule,
+	RouterModule.forRoot([
+	{path: '', component:
+	MainComponent}
+	]),
   ],
   providers: [],
   bootstrap: [AppComponent]
